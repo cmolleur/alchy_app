44bcdebe5ff2913ed55cad6ce411801d428800d2
Colette Molleur <colette.molleur@gmail.com>
2016-05-23 16:15:14 -0400

    adds scripts file

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -59,16 +59,16 @@
 //
 //
 //
-function submitForm() {
-   // Get the first form with the name
-   // Hopefully there is only one, but there are more, select the correct index
-   var frm = document.getElementsByName('form')[0];
-   frm.submit(); // Submit
-   frm.reset();  // Reset
-   return false; // Prevent page refresh
-}
-$(document).ready(function(){
-  submitForm();
+// function submitForm() {
+//    // Get the first form with the name
+//    // Hopefully there is only one, but there are more, select the correct index
+//    var frm = document.getElementsByName('form')[0];
+//    frm.submit(); // Submit
+//    frm.reset();  // Reset
+//    return false; // Prevent page refresh
+// }
+// $(document).ready(function(){
+//   submitForm();
 
 //   $(".delete-btn").mousedown(function(){
 //     $(this).css("background-color", "white");
